6c4a9e528a8e91a0279ca6b4ade19a35cb8351c5
Marat <brutto061@gmail.com>
2022-10-22 13:00:38 +0700

    Выполнили Задание №2

---
+++ zadanie_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число строк: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число столбцов: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] matrix = new int[m, n];
+for(int i=0; i<matrix.GetLength(0); i++)
+{
+    for(int j=0; j<matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = i + j;
+        Console.Write($"{matrix[i, j]} ");
+    }
+Console.WriteLine();    
+}
